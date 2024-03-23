9822066d485e18f27523133eadad3b4dbec758a5
5ms <oms010414@gmail.com>
2024-03-23 20:29:50 +0900

    메뉴얼 수정, 버그 수정

--- config/helmetConfig.js
+++ config/helmetConfig.js
@@ -1,7 +1,7 @@
 const helmetConfig = {
     contentSecurityPolicy: {
         directives: {
-            defaultSrc: ["'self'"],
+            defaultSrc: ["'self'", "cdn.jsdelivr.net", "www.gstatic.com", "localhost", "http://localhost"],
             scriptSrc: ["'self'", "cdn.jsdelivr.net", "www.gstatic.com", "localhost", "http://localhost"],
             objectSrc: ["'none'"],
             'form-action': ["*"],
--- public/script/requestNotification.js
+++ public/script/requestNotification.js
@@ -1,9 +1,13 @@
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
     if (!("Notification" in window)) {
         alert("이 브라우저는 웹 알림을 지원하지 않습니다.");
     } 
     else {
-        Notification.requestPermission();
+        Notification.requestPermission(permission=>{
+            if(permission=="denied"){
+                alert("알람 권한을 허용해주세요!");
+            }
+        });
     };
 });

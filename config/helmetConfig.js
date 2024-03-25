bcd43aaa9b683785abfdf4864ecdbe8716a95e2c
minsuk <oms010414@gmail.com>
2024-03-25 18:02:54 +0900

    helmet 설정 수정

--- config/helmetConfig.js
+++ config/helmetConfig.js
@@ -26,7 +26,7 @@ const helmetConfig = {
                 "https://fcmregistrations.googleapis.com/*", 
                 "https://firebaseinstallations.googleapis.com/*",
                 "https://fcmregistrations.googleapis.com/*",
-                "https://fcmregistrations.googleapis.com/v1/projects/attention-b4db3/registrations/",
+                "https://fcmregistrations.googleapis.com/v1/projects/attention-b4db3/registrations",
                 "https://fcmregistrations.googleapis.com/v1/projects/attention-b4db3/registrations/e7rSCMEuHxaPeSneUnPb34:APA91bGiex14-K-bme9Ag5IW-i8VlqfqKellU4qTBRSKibaaC43QBcWXtZfKeaasOgE3LAZlA-zi4eFNfYkyU2pEXhvIhl0VxIRjVn0Ow3hEeeYYaPYMuf8lkXhfBHXoyk1qRk8bTOM6",
                 "https://firebaseinstallations.googleapis.com/v1/projects/attention-b4db3/installations"
             ],

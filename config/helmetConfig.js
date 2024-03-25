b9fd67de4c34f67fb13ed231541d44d2bfbe3d90
minsuk <oms010414@gmail.com>
2024-03-25 13:36:47 +0900

    버그 수정

--- config/helmetConfig.js
+++ config/helmetConfig.js
@@ -1,8 +1,8 @@
 const helmetConfig = {
     contentSecurityPolicy: {
         directives: {
-            defaultSrc: ["'self'", "cdn.jsdelivr.net", "www.gstatic.com", "localhost", "http://localhost", "https://omsoms.kr/*"],
-            scriptSrc: ["'self'", "cdn.jsdelivr.net", "www.gstatic.com", "localhost", "http://localhost", "https://omsoms.kr/*"],
+            defaultSrc: ["'self'", "cdn.jsdelivr.net", "www.gstatic.com", "localhost", "http://localhost", "https://omsoms.kr/*", "firebaseinstallations.googleapis.com"],
+            scriptSrc: ["'self'", "cdn.jsdelivr.net", "www.gstatic.com", "localhost", "http://localhost", "https://omsoms.kr/*", "firebaseinstallations.googleapis.com"],
             objectSrc: ["'none'"],
             'form-action': ["*"],
             upgradeInsecureRequests: [],
